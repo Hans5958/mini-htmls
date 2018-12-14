1603ebfff9eaddc7a955754b0f6ce8ca70a80b4d
Hans5958 <email5958.1@gmail.com>
2018-12-14 22:23:59 +0700

    minor changes + battle pages

--- data/require.js
+++ data/require.js
@@ -2,11 +2,12 @@ $(function(){
   $("head").append("<style>html{position:relative;min-height:100%}body{margin-bottom:60px}.footer{position:absolute;bottom:0;width:100%;height:60px;line-height:60px;background-color:#f5f5f5}</style>")
   $("body").prepend("<span id=\"nav\">")
   $("body").append("<span id=\"footer\">")
-  $("#nav").load("data/nav.html")
-  $("#footer").load("data/footer.html")
+  $("#nav").load("/mini-htmls/data/nav.html")
+  $("#footer").load("/mini-htmls/data/footer.html")
   $("body").append("<div id=\"require\">")
-  $("#require").load("data/require.html")
+  $("#require").load("/mini-htmls/data/require.html",function(){$("head")[0].innerHTML+=$("#require2").html(),$("#require")[0].outerHTML=""})
   Pace.on("done", function() {
-    $('#overlay').delay(300).fadeOut(600);
+    $('#overlay').delay(300).fadeOut(600)
+	setTimeout(function(){Pace.options.ajax = false}, 300)
  });
 })
