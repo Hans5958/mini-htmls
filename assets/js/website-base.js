117bb59be2941510f0cd26f952b4e3ad77acc3f9
Hans5958 <email5958.1@gmail.com>
2020-08-31 23:50:56 +0700

    Use Iconify and layout changes

--- assets/js/website-base.js
+++ assets/js/website-base.js
@@ -4,28 +4,24 @@
  * Licensed under the GPL license
  */
 
-$(document).ready(function () {
-	$(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
-		var $el = $(this)
-		var $parent = $(this).offsetParent(".dropdown-menu")
+$(() => {
+	$(".dropdown-menu a.dropdown-toggle").on("click", (e) => {
+		var el = $(this)
+		var parent = $(this).offsetParent(".dropdown-menu")
 		if (!$(this).next().hasClass("show")) {
 			$(this).parents(".dropdown-menu").first().find(".show").removeClass("show")
 		}
-		var $subMenu = $(this).next(".dropdown-menu")
-		$subMenu.toggleClass("show")
-
+		var subMenu = $(this).next(".dropdown-menu")
+		subMenu.toggleClass("show")
 		$(this).parent("li").toggleClass("show")
-
 		$(this)
 			.parents("li.nav-item.dropdown.show")
-			.on("hidden.bs.dropdown", function (e) {
+			.on("hidden.bs.dropdown", (e) => {
 				$(".dropdown-menu .show").removeClass("show")
 			})
-
-		if (!$parent.parent().hasClass("navbar-nav")) {
-			$el.next().css({ top: $el[0].offsetTop, left: $parent.outerWidth() - 4 })
+		if (!parent.parent().hasClass("navbar-nav")) {
+			el.next().css({ top: el[0].offsetTop, left: parent.outerWidth() - 4 })
 		}
-
 		return false
 	})
 })
