2197977055addb976ce63c47510fe0ac2ce816ae
Hans5958 <email5958.1@gmail.com>
2021-05-18 21:27:24 +0700

    Swap multi-level nav code with another one

--- site/assets/js/base.js
+++ site/assets/js/base.js
@@ -1,27 +1,17 @@
-/*!
- * Bootstrap 4 multi dropdown navbar (https://github.com/bootstrapthemesco/bootstrap-4-multi-dropdown-navbar)
- * Copyright 2017.
- * Licensed under the GPL license
- */
+/*
+Multi-level dropdown on header navigation bar
+https://stackoverflow.com/a/45755948
+*/
 
 $(() => {
-	$(".dropdown-menu a.dropdown-toggle").on("click", (e) => {
-		var el = $(this)
-		var parent = $(this).offsetParent(".dropdown-menu")
-		if (!$(this).next().hasClass("show")) {
-			$(this).parents(".dropdown-menu").first().find(".show").removeClass("show")
+	$('.dropdown-menu a.dropdown-toggle').on('click', e => {
+		if (!$(this).next().hasClass('show')) {
+			$(this).parents('.dropdown-menu').first().find('.show').removeClass('show')
 		}
-		var subMenu = $(this).next(".dropdown-menu")
-		subMenu.toggleClass("show")
-		$(this).parent("li").toggleClass("show")
-		$(this)
-			.parents("li.nav-item.dropdown.show")
-			.on("hidden.bs.dropdown", (e) => {
-				$(".dropdown-menu .show").removeClass("show")
-			})
-		if (!parent.parent().hasClass("navbar-nav")) {
-			el.next().css({ top: el[0].offsetTop, left: parent.outerWidth() - 4 })
-		}
+		$(this).next('.dropdown-menu').toggleClass('show')
+		$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', e => {
+		 	$('.dropdown-menu .show').removeClass('show')
+		})
 		return false
 	})
 })
