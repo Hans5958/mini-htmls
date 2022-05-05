79619dd9a2e6a5fb6a5664e3399deba973746ef5
Hans5958 <hans5958@outlook.com>
2022-05-05 08:49:33 +0700

    Start experimenting with PWA 2

--- site/tnkb-analyzer/pwa-sw.js
+++ site/tnkb-analyzer/pwa-sw.js
@@ -3,11 +3,12 @@ layout: pwa-sw-js
 title: tnkb-analyzer
 ---
 
-{{ '/tnkb-analyzer/dictMerged.json' | absolute_url }}
-{{ '/tnkb-analyzer/dictMergedEnglish.json' | absolute_url }}
+{{ '/tnkb-analyzer/' | absolute_url }}
+{{ '/tnkb-analyzer/index.html' | absolute_url }}
 {{ '/tnkb-analyzer/en.html' | absolute_url }}
 {{ '/tnkb-analyzer/en' | absolute_url }}
-{{ '/tnkb-analyzer/index.html' | absolute_url }}
 {{ '/tnkb-analyzer/script.js' | absolute_url }}
 {{ '/tnkb-analyzer/style.css' | absolute_url }}
-{{ '/tnkb-analyzer/' | absolute_url }}
+{{ '/tnkb-analyzer/dictMerged.json' | absolute_url }}
+{{ '/tnkb-analyzer/dictMergedEnglish.json' | absolute_url }}
+{{ '/tnkb-analyzer/pwa.js' | absolute_url }}
