37734a51e4dc024cc01445957727d2c2ca503f4e
Hans5958 <hans5958@outlook.com>
2022-05-05 15:02:58 +0700

    Improvements on PWA 2

--- site/tnkb-analyzer/pwa-sw.js
+++ site/tnkb-analyzer/pwa-sw.js
@@ -5,8 +5,8 @@ id: tnkb-analyzer
 
 {{ '/tnkb-analyzer/' | absolute_url }}
 {{ '/tnkb-analyzer/index.html' | absolute_url }}
-{{ '/tnkb-analyzer/en.html' | absolute_url }}
 {{ '/tnkb-analyzer/en' | absolute_url }}
+{{ '/tnkb-analyzer/en.html' | absolute_url }}
 {{ '/tnkb-analyzer/script.js' | absolute_url }}
 {{ '/tnkb-analyzer/style.css' | absolute_url }}
 {{ '/tnkb-analyzer/dictMerged.json' | absolute_url }}
