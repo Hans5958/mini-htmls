4a83706b31d460d67b23f34efe6102fe50b02a84
Hans5958 <hans5958@outlook.com>
2022-05-05 14:15:11 +0700

    My Minecraft Skins: Support PWA

---
+++ site/my-minecraft-skins/pwa-sw.js
@@ -0,0 +1,16 @@
+---
+layout: pwa-sw-js
+id: my-minecraft-skins
+---
+
+{{ '/my-minecraft-skins/' | absolute_url }}
+{{ '/my-minecraft-skins/index.html' | absolute_url }}
+{{ '/my-minecraft-skins/skins/computer-guy.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/f3455-abandonbot.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/gameboy.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/p-rill.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/prototypes.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/redstone-engineer.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/redstone.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/redstoner.png' | absolute_url }}
+{{ '/my-minecraft-skins/skins/speedycube.png' | absolute_url }}
