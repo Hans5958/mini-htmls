09683d6068037c816a62e420367d538718021c6c
Hans5958 <hans5958@outlook.com>
2022-09-01 23:01:11 +0700

    COVID Counter: Yet another refactor

--- site/covid-counter/script.js
+++ site/covid-counter/script.js
@@ -1,4 +1,8 @@
-let varsToCheck = [], varsChecked = 0, timer = 0, statusProgressBar = "fetch", isCors = true
+let countersToCheck = [], 
+	countersChecked = 0, 
+	statusProgressBar = "fetch", 
+	isCors = true
+const delay = 10000
 
 var parseToHTML = (raw) => (new DOMParser()).parseFromString(raw, 'text/html')
 
@@ -18,33 +22,43 @@ const getData = (url, type, done = () => {}, fail = () => {}, replied = () => {}
 		})
 }
 
+const counters = []
+const addCounter = counter => {
+	const index = counters
+	counters.push(new Counter(counter, index))
+}
+
 class Counter {
 	constructor({
 		name,
 		elementID,
 		url,
-		doneCallback = () => { }, 
-		failCallback = () => { }, 
-		repliedCallback = () => { }, 
-		applyCallback = () => { }, 
-		resetCallback = () => { },
+		callback,
 		values
-	}) {
+	},
+	index) {
+		this.index = index
 		this.name = name
 		this.elementID = elementID
 		this.url = url
-		this.doneCallback = doneCallback
-		this.failCallback = failCallback
-		this.repliedCallback = repliedCallback
+		this.callback = {
+			done: () => {}, 
+			fail: () => {}, 
+			replied: () => {}, 
+			apply: () => {}, 
+			reset: () => {},
+		}
+		if (callback) this.callback = {
+			...this.callback,
+			...callback
+		}
 		this.values = values || {}
-		this.applyCallback = applyCallback
-		this.resetCallback = resetCallback
 		this.section = document.querySelector(`#${this.elementID}.section`)
-		this.attach()
+		this.attached = true
 	}
 
 	apply() {
-		this.applyCallback()
+		this.callback.apply()
 		const section = this.section
 		const { currentConfirmed, currentDeaths, currentRecovered } = this.values
 		section.querySelector(`.counter-main`).textContent = currentConfirmed
@@ -58,7 +72,7 @@ class Counter {
 	}
 
 	reset() {
-		this.resetCallback()
+		this.callback.reset()
 		this.values = {}
 	}
 
@@ -80,34 +94,31 @@ class Counter {
 				getData(
 					...urlToFetch,
 					async (data) => {
-						var newValues = this.doneCallback(data, urlToFetch[0])
-						Object.keys(newValues).forEach(key => {
-							if (this.values[key] === undefined || this.values[key] < newValues[key]) {
-								this.values[key] = newValues[key]
-							}
-						})
+						let newValues = data
+						newValues = this.callback.done(data, urlToFetch[0])
+						this.values = newValues
 					},
-					this.failCallback,
+					this.callback.fail,
 					() => {
-						this.repliedCallback()
+						this.callback.replied()
 						callback()
 					}
 				)	
 			} else {
-				this.failCallback()
-				this.repliedCallback()
+				this.callback.fail()
+				this.callback.replied()
 				callback()
 			}
 		})
 	}
 
 	attach() {
-		varsToCheck = varsToCheck.filter(e => e !== this.name).concat(this.name)
+		this.attached = true
 		this.section.style.display = ""
 	}
 
 	detach() {
-		varsToCheck = varsToCheck.filter(e => e !== this.name)
+		this.attached = false
 		if (Object.keys(this.values).length === 0) {
 			this.section.style.display = "none"
 			this.apply()
@@ -115,41 +126,44 @@ class Counter {
 	}
 
 	static applyAll() {
-		varsToCheck.forEach(value => {
-			window[value].apply()
-		})
+		const attachedCounters = counters.filter(counter => counter.attached)
+		for (const i in attachedCounters) {
+			const counter = attachedCounters[i]
+			counter.apply()
+		}
 	}
 
 	static resetAll() {
-		varsToCheck.forEach(value => {
-			window[value].reset()
-		})
+		const attachedCounters = counters.filter(counter => counter.attached)
+		for (const i in attachedCounters) {
+			const counter = attachedCounters[i]
+			counter.reset()
+		}
 	}
-
+	
 	static async fetchAll() {
-		return new Promise(async callback => {
-			varsToCheck.forEach(async value => {
-				document.querySelector("#status .progress-bar").style.transition = "width .25s ease"
-				await window[value].fetch()
-				varsChecked++
-				document.querySelector("#status p").textContent = `Fetching data... (${varsChecked}/${varsToCheck.length})`
-				document.querySelector("#status .progress-bar").style.width = `${(varsChecked / varsToCheck.length) * 100}%`
-				if (varsChecked === varsToCheck.length) {
-					varsChecked = 0
-					document.querySelector("#status p").textContent = "All data fetched!"
-					setTimeout(() => {
-						document.querySelector("#status .progress-bar").style.transition = "unset"
-						statusProgressBar = "timer"
-					}, 1000)
-					callback()
-				}
+		const attachedCounters = counters.filter(counter => counter.attached)
+		document.querySelector("#status p").textContent = `Fetching data... (0/${attachedCounters.length})`
+		document.querySelector("#status .progress-bar").style.transition = "width .25s ease"
+		await Promise.all(
+			attachedCounters.map(async counter => {
+				await counter.fetch()
+				countersChecked++
+				document.querySelector("#status p").textContent = `Fetching data... (${countersChecked}/${attachedCounters.length})`
+				document.querySelector("#status .progress-bar").style.width = `${(countersChecked / attachedCounters.length) * 100}%`
 			})
-		})
+		)
+		countersChecked = 0
+		document.querySelector("#status p").textContent = "All data fetched!"
+		setTimeout(() => {
+			document.querySelector("#status .progress-bar").style.transition = "unset"
+			statusProgressBar = "timer"
+		}, 1000)
 	}
 
 }
 
-var worldometers = new Counter({
+addCounter({
 	name: "worldometers", 
 	elementID: "worldometers", 
 	url: {
@@ -160,39 +174,41 @@ var worldometers = new Counter({
 			["https://disease.sh/v3/covid-19/all", "json"]
 		]
 	},
-	doneCallback: (data, urlToFetch) => {
-		if (urlToFetch === "https://www.worldometers.info/coronavirus/") {
-			var returnedObj = {
-				"currentConfirmed": parseInt(data.querySelector(".content-inner > div:nth-child(7) > div:nth-child(2) > span").textContent.split(",").join("")),
-				"currentDeaths": parseInt(data.querySelector(".content-inner > div:nth-child(9) > div:nth-child(2) > span").textContent.split(",").join("")),
-				"currentRecovered": parseInt(data.querySelector(".content-inner > div:nth-child(10) > div:nth-child(2) > span").textContent.split(",").join("")),
-				"lastUpdated": new Date()
+	callback: {
+		done: (data, urlToFetch) => {
+			if (urlToFetch === "https://www.worldometers.info/coronavirus/") {
+				var returnedObj = {
+					"currentConfirmed": parseInt(data.querySelector(".content-inner > div:nth-child(7) > div:nth-child(2) > span").textContent.split(",").join("")),
+					"currentDeaths": parseInt(data.querySelector(".content-inner > div:nth-child(9) > div:nth-child(2) > span").textContent.split(",").join("")),
+					"currentRecovered": parseInt(data.querySelector(".content-inner > div:nth-child(10) > div:nth-child(2) > span").textContent.split(",").join("")),
+					"lastUpdated": new Date()
+				}
+				if (worldometers.values === {} ? false : worldometers.values.currentConfirmed === returnedObj.currentConfirmed) {
+					return worldometers.values
+				} else {
+					return returnedObj
+				}	
+			} else if (urlToFetch === "https://disease.sh/v3/covid-19/all") {
+				return {
+					"currentConfirmed": data.cases,
+					"currentDeaths": data.deaths,
+					"currentRecovered": data.recovered,
+					"lastUpdated": new Date()
+				}
 			}
-			if (worldometers.values === {} ? false : worldometers.values.currentConfirmed === returnedObj.currentConfirmed) {
-				return worldometers.values
-			} else {
-				return returnedObj
-			}	
-		} else if (urlToFetch === "https://disease.sh/v3/covid-19/all") {
-			return {
-				"currentConfirmed": data.cases,
-				"currentDeaths": data.deaths,
-				"currentRecovered": data.recovered,
-				"lastUpdated": new Date()
-			}
+		},
+		apply: () => {
+			getData("https://disease.sh/v3/covid-19/all", "json", (data) => {
+				this.values.lastUpdated = new Date(data.updated)
+				this.callback.apply = () => { }
+				this.apply()
+			})
 		}
-	},
-	applyCallback: () => {
-		getData("https://disease.sh/v3/covid-19/all", "json", (data) => {
-			worldometers.values.lastUpdated = new Date(data.updated)
-			worldometers.applyCallback = () => { }
-			worldometers.apply()
-		})
 	}
 })
 
 
-var JohnsHopkins = new Counter({
+addCounter({
 	name: "JohnsHopkins", 
 	elementID: "johnshopkins",
 	url: {
@@ -200,28 +216,30 @@ var JohnsHopkins = new Counter({
 			["https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&outFields=%22Last_Update,Confirmed,Deaths,Recovered%22&returnGeometry=false", "json"]
 		]
 	},
-	doneCallback: (data, urlToFetch) => {
-		resultObj = {
-			"currentConfirmed": 0,
-			"currentDeaths": 0,
-			"currentRecovered": 0,
-			"lastUpdated": 0
-		}
-		data.features.forEach(value => {
-			resultObj.currentConfirmed += value.attributes.Confirmed
-			resultObj.currentDeaths += value.attributes.Deaths
-			resultObj.currentRecovered += value.attributes.Recovered
-			if (resultObj.lastUpdated < value.attributes.Last_Update) {
-				resultObj.lastUpdated = value.attributes.Last_Update
+	callback: {
+		done: (data, urlToFetch) => {
+			resultObj = {
+				"currentConfirmed": 0,
+				"currentDeaths": 0,
+				"currentRecovered": 0,
+				"lastUpdated": 0
 			}
-		})
-
-		resultObj.lastUpdated = new Date(resultObj.lastUpdated)
-		return resultObj
+			data.features.forEach(value => {
+				resultObj.currentConfirmed += value.attributes.Confirmed
+				resultObj.currentDeaths += value.attributes.Deaths
+				resultObj.currentRecovered += value.attributes.Recovered
+				if (resultObj.lastUpdated < value.attributes.Last_Update) {
+					resultObj.lastUpdated = value.attributes.Last_Update
+				}
+			})
+	
+			resultObj.lastUpdated = new Date(resultObj.lastUpdated)
+			return resultObj
+		}
 	}
 })
 
-var Indonesia = new Counter({
+addCounter({
 	name: "Indonesia", 
 	elementID: "indonesia",
 	url: {
@@ -229,17 +247,19 @@ var Indonesia = new Counter({
 			["https://data.covid19.go.id/public/api/update.json", "json"]
 		]
 	},
-	doneCallback: (data) => {
-		return {
-			"currentConfirmed": data.update.total.jumlah_positif,
-			"currentDeaths": data.update.total.jumlah_meninggal,
-			"currentRecovered": data.update.total.jumlah_sembuh,
-			"lastUpdated": new Date(data.update.penambahan.created)
+	callback: {
+		done: data => {
+			return {
+				"currentConfirmed": data.update.total.jumlah_positif,
+				"currentDeaths": data.update.total.jumlah_meninggal,
+				"currentRecovered": data.update.total.jumlah_sembuh,
+				"lastUpdated": new Date(data.update.penambahan.created)
+			}
 		}
-	},
+	}
 })
 
-var IHME = new Counter({
+addCounter({
 	name: "IHME", 
 	elementID: "ihme",
 	url: {
@@ -247,24 +267,26 @@ var IHME = new Counter({
 			["https://healthmap.org/covid-19/latestCounts.json", "json"]
 		]
 	},
-	doneCallback: (data) => {
-		if (IHME.values === {} ? false : IHME.values.currentConfirmed === parseInt(data[0].caseCount.split(",").join(""))) {
-			return IHME.values
-		} else {
-			return {
-				"currentConfirmed": parseInt(data[0].caseCount.split(",").join("")),
-				"currentDeaths": 0,
-				"currentRecovered": 0,
-				"lastUpdated": new Date()
+	callback: {
+		done: (data) => {
+			if (this.values === {} ? false : this.values.currentConfirmed === parseInt(data[0].caseCount.split(",").join(""))) {
+				return this.values
+			} else {
+				return {
+					"currentConfirmed": parseInt(data[0].caseCount.split(",").join("")),
+					"currentDeaths": 0,
+					"currentRecovered": 0,
+					"lastUpdated": new Date()
+				}
 			}
+		},
+		apply: () => {
+			getData("https://api.github.com/repos/beoutbreakprepared/nCoV2019/commits", "json", (data) => {
+				this.values.lastUpdated = new Date(data[0].commit.committer.date)
+				this.callback.apply = () => { }
+				this.apply()
+			})
 		}
-	},
-	applyCallback: () => {
-		getData("https://api.github.com/repos/beoutbreakprepared/nCoV2019/commits", "json", (data) => {
-			IHME.values.lastUpdated = new Date(data[0].commit.committer.date)
-			IHME.applyCallback = () => { }
-			IHME.apply()
-		})
 	},
 	values: {
 		"currentConfirmed": 71503614,
@@ -283,31 +305,28 @@ $(document).ready(async () => {
 	getData("https://en.wikipedia.org/w/api.php", "html", () => {
 	}, () => {
 		isCors = false
-		Indonesia.detach()
+		counters.forEach(counter => {
+			if (!counter.url.cors && !counter.values.currentConfirmed) counter.detach()
+		})
 	}, () => {
 		// Counter.resetAll()
 		execute()
-		setInterval(
+		let targetTime = Date.now() + delay
+		const timerUpdate = setInterval(
 			() => {
-				timer++
+				let timer = (targetTime - Date.now())/1000
 				if (statusProgressBar === "timer") {
-					document.querySelector("#status p").textContent = `Waiting... (${((1000 - timer) / 100).toFixed(2)}s)`
-					document.querySelector("#status .progress-bar").style.width = `${(timer / 1000) * 100}%`
+					document.querySelector("#status p").textContent = `Waiting... (${timer.toFixed(2)}s)`
+					document.querySelector("#status .progress-bar").style.width = `${100 - (timer / 10) * 100}%`
 				}
-				if (timer === 1000) {
+				if (timer < 0 || timer === 0) {
 					statusProgressBar = "fetch"
-					document.querySelector("#status p").textContent = `Fetching data... (0/${varsToCheck.length})`
 					document.querySelector("#status .progress-bar").style.width = "0"
-					timer = 0
+					targetTime = Date.now() + delay
 					execute()
 				}
 			}, 10
 		)
-		odometerStyle = document.querySelector("#odometer-style").textContent
-		document.querySelector("#odometer-style").textContent = ""
-		setTimeout(() => {
-			document.querySelector("#odometer-style").textContent = odometerStyle
-		}, 10000)
 	})
 })
 
