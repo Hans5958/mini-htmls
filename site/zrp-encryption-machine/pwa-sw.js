11bb53581b3d0cf80b638630c1e7fd178462ad2d
Hans5958 <hans5958@outlook.com>
2023-01-01 02:15:04 +0700

    .zrp Encryption Machine: Forgot adding resource to prefetch

--- site/zrp-encryption-machine/pwa-sw.js
+++ site/zrp-encryption-machine/pwa-sw.js
@@ -6,3 +6,4 @@ id: zrp-encryption-machine
 {{ '/zrp-encryption-machine/' | absolute_url }}
 {{ '/zrp-encryption-machine/index.html' | absolute_url }}
 {{ '/zrp-encryption-machine/script.js' | absolute_url }}
+https://cdn.jsdelivr.net/npm/aes-js@3/index.min.js
